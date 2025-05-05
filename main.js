a9ad304d049bb3a11c04774f7b7c2dc6dbb9dfa7
Rafaelhenriquereyrey11tia reyrey <rhreyrey@gmail.com>
2025-05-05 01:35:06 +0000

    esqueci dos titulos

---
+++ main.js
@@ -0,0 +1,71 @@
+ /*-1
+ function mensagem() {
+    console.log("Seja bem-vindo(a)!")
+    console.log("É um prazer tê-lo aqui.")
+    console.log("Aproveite")
+}*/
+
+
+
+
+/*-2
+function somarNumeros(valor1, valor2) {
+    let soma = valor1 + valor2
+    console.log(soma)
+}
+
+somarNumeros(15, 27)*/
+
+
+
+
+/*-3
+function boasVindas(nome, cargo) {
+    console.log(`Seja bem-vindo(a) ${cargo} ${nome}!`)
+}
+
+boasVindas("Marcio", "professor")
+*/
+
+
+
+
+
+/*-4
+function somar(valor1, valor2) {
+    return valor1 + valor2
+}
+
+let resultado = somar(29, 32)
+
+console.log(somar(12, 12))
+console.log(resultado)*/
+
+
+
+
+
+/*-5
+function boasVindas(nome, cargo) {
+    return `Seja bem-vindo(a) ${cargo} ${nome}!`
+}
+
+let mensagem = boasVindas("Marcio", "professor")
+
+console.log(mensagem)*/
+
+
+
+/*-6
+function parImpar(valor) {
+    if (valor % 2 === 0) {
+        return "par"
+    }
+    return "impar"
+}
+
+let verificacao = parImpar(16)
+
+console.log(verificacao)
+*/
+
